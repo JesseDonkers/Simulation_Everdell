77db598b585471675ce12f07abec8ecee93135ff
Jesse Donkers <jesse.donkers@live.nl>
2026-01-15 16:19:42 +0100

    Created card class

---
+++ Class_Card.py
@@ -0,0 +1,25 @@
+class Card:
+    def __init__(self):
+        self.name = ""
+        self.color = ""
+        self.requirements = dict(twig=0, resin=0, pebble=0, berry=0)
+        self.unique = False
+        self.cardsindeck = 0
+        self.points = 0
+        
+    def __str__(self):
+        return str(self.name)
+
+class Critter(Card):
+    def __init__(self):
+        super().__init__()
+        self.relatedcontruction = ""
+
+class Construction(Card):
+    def __init__(self):
+        super().__init__()
+        self.relatedcritter = ""
+        self.relatedoccupied = False
+        self.open = False
+        self.workers = 0
+        self.maxworkers = 0
